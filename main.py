ea2255090e287b55a5101923939b20e7c77fb468
Andrew Chepreghy <andris788@gmail.com>
2019-02-18 15:40:09 +0100

    cleaned up main.py from assignment related things

--- main.py
+++ main.py
@@ -8,16 +8,7 @@ import matplotlib.ticker as ticker
 
 from plotify import Plotify
 
-# age – a positive integer (years)
-# FEV1 – a continuous valued measurement(liter)
-# height – a continuous valued measurement(inches) 
-# gender – binary(female: 0, male: 1)
-# smoking status – binary(non-smoker: 0, smoker: 1) 
-# weight – a continuous valued measurement(kg)
-
-# Exercise 1: 
-# - Divide data into smokers and non-smokers
-# - Calculate the mean FEV1 score for both group
+# Data preprocessing
 
 smoking_data = np.loadtxt('smoking.txt', delimiter="\t")
 
@@ -30,25 +21,17 @@ for person in smoking_data:
   elif person[4] == 1:
     smokers.append(person)
 
-print(np.array(smokers))
 smokers = np.array(smokers)
 nonsmokers = np.array(nonsmokers)
 
 smokers_FEV1 = smokers[:,1]
 nonsmokers_FEV1 = nonsmokers[:,1]
 
-print('number of smoker FEV1 scores ' + str(len(smokers_FEV1)))
-print('number of non-smoker FEV1 scores ' + str(len(nonsmokers_FEV1)))
- 
-smokers_avg_FEV1 = np.mean(smokers_FEV1)
-nonsmokers_avg_FEV1 = np.mean(nonsmokers_FEV1)
-
-print("Smokers' FEV1 score: ", str(smokers_avg_FEV1))
-print("Non-smokers' FEV1 score: ", str(nonsmokers_avg_FEV1))
-
-# Exercise 2
+# Initialize plotify
 
 plotify = Plotify()
+
+# Boxplot
 
 plotify.boxplot(
     data=[smokers_FEV1, nonsmokers_FEV1],
@@ -57,32 +40,7 @@ plotify.boxplot(
     title='FEV1 scores for smokers\nand non-smokers'
 )
 
-
-# Exercise 3
-
-variance_smokers = smokers_FEV1.var(ddof=1)
-variance_nonsmokers = nonsmokers_FEV1.var(ddof=1)
-print(variance_smokers)
-print(variance_nonsmokers)
-
-standard_deviation = np.sqrt((variance_smokers + variance_nonsmokers)/2)
-
-print(standard_deviation)
-
-# t = (smokers_FEV1.mean() - nonsmokers_FEV1.mean())/(standard_deviation*np.sqrt(2/N))
-
-alpha = 0.05
-
-t, p = stats.ttest_ind(smokers_FEV1, nonsmokers_FEV1, equal_var=False)
-
-print('t is: ', t)
-print('p is: ', p)
-
-if p < alpha:
-  print('Null hypothesis is rejected. The likeliness of it being true is: ' + str(p*100) + '%')
-
-if p > alpha:
-  print('Null hypothesis is acceoted.')
+# Scatter plot
 
 smokers_x = smokers[:,0]
 smokers_y = smokers[:, 1]
@@ -101,7 +59,7 @@ plotify.scatter_plot(
   legend_labels = ('Non-smokers', 'Smokers')
 )
 
-# Exercise 5
+# Histogram
 
 plotify.histogram(
   x_list = [nonsmokers_x, smokers_x],
